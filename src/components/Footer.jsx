27e95a94af2833c8042b6c84cbdec59055cc5518
dladdu <dladduwps@gmail.com>
2025-09-07 16:11:08 -0400

    1.1.7

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ import { ArrowUp } from "lucide-react";
 export const Footer = () => {
     return (
         <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8">
-            <div className="flex flex-col items-center justify-center max-w-5xl mx-auto">
+            <div className="max-w-5xl mx-auto">
                 <p className="text-sm text-muted-foreground mb-4 text-center">
                     &copy; {new Date().getFullYear()} devan-laddu-portfolio.vercel.app. All rights reserved.
                 </p>
 
-                <a href="#hero" className="flex items-center justify-center p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors">
+                <a href="#hero" className="absolute top-4 right-4 p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors">
                     <ArrowUp size={20}/>
                 </a>
             </div>
